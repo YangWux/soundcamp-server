6fd4365cc65d12ee9cf16b3f0e581246ac8593b8
wemoon1 <wemoon@ucsc.edu>
2018-02-09 20:20:57 -0800

    test env var works

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const PORT = process.env.PORT || 4200;
+const SONGKICK_API = process.env.TEST_VAR;
 
 // the middleware
 router.use(function(req, res, next) {
@@ -11,7 +12,7 @@ router.use(function(req, res, next) {
 
 // define our routes
 router.get('/', function(req, res) {
-  res.json({msg: 'it works'});
+  res.json({msg: SONGKICK_API});
 });
 
 // prefix above routes with /api
